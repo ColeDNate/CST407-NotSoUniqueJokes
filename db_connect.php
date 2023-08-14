e49dee560ed8f5a35fd50515dd25ede3bb020501
ColeDNate <colednate@gmail.com>
2023-08-14 12:10:41 -0700

    Update db_connect.php
    updating hostname to sql server

--- db_connect.php
+++ db_connect.php
@@ -4,7 +4,7 @@ ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
 // modify these settings according to the account on your database server.
-$host = "notsouniquejokes.azurewebsites.net";
+$host = "jokes-database-server.mysql.database.azure.com";
 $port = "3306";
 $username = "JokesAdmin";
 $user_pass = "LopesLeap42";
@@ -16,7 +16,9 @@ mysqli_real_connect($conn, $host, $username, $user_pass, $database_in_use, 3306)
 if (mysqli_connect_errno($conn)) {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
+else{
+    echo "Connection successful.<br>";
+}
 
-echo $mysqli->host_info . "<br>";
 
 ?>
