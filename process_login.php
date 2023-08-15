f76cc2a1a651e67a37a4b8107705d348fe3ae844
ColeDNate <colednate@gmail.com>
2023-08-14 19:16:42 -0700

    Update process_login.php
    Initial change to process azure sql

--- process_login.php
+++ process_login.php
@@ -14,17 +14,23 @@ $password = $_POST['password'];
 
 echo "<h2>You attempted to login with " . $username . " and " . $password . "</h2>";
 
-$stmt = $mysqli->prepare ("SELECT user_id, user_name, password FROM users WHERE user_name = ?");
+$sql = "SELECT user_id, user_name, password FROM users WHERE user_name = ?";
+
+$stmt = mysqli_prepare ($conn, $sql);
 $stmt->bind_param("s", $username);
 
-$stmt->execute();
-$stmt->store_result();
-
-$stmt->bind_result($userid, $fetched_name, $fetched_pass);
+mysqli_stmt_execute($stmt);
+$result = $stmt->get_result();
 
 if ($stmt->num_rows > 0 ) {
     echo "Found 1 person with that username<br>";
-    $stmt->fetch();
+
+    // Fetch the result row before binding results
+    $row = $result->fetch_assoc();
+    $userid = $row['user_id'];
+    $fetched_name = $row['user_name'];
+    $fetched_pass = $row['password'];
+ 
     if (password_verify($password, $fetched_pass)) {
         echo "The password matches<br>";
         echo "Login success<br>"; 
